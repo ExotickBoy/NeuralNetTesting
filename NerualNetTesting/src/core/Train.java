a48e9f6609dd7a2182ea43830481a4edd33acaa7
Kacper Lubisz <kacperlubisz@gmail.com>
2017-04-15 23:22:51 +0100

    Introduced variable hidden layers

--- NerualNetTesting/src/core/Train.java
+++ NerualNetTesting/src/core/Train.java
@@ -19,9 +19,10 @@ public class Train {
 	private static final int TEST_SAMPLES = 10000;
 	
 	private static final int HIDDEN_LAYER_SIZE = 1000;
+	private static final int HIDDEN_LAYER_AMOUNT = 3;
 	
-	private static final double LEARNING_RATE = .000005;;
-	private static final double SAMPLE_PROPORTION = 0.01;
+	private static final double LEARNING_RATE = 1;
+	private static final double SAMPLE_PROPORTION = 0.002;
 	
 	private static long time = 0;
 	
@@ -48,7 +49,7 @@ public class Train {
 		}
 		
 		Random r = new Random(0);
-		NN network = new NN(x.getColumns(), y.getColumns(), HIDDEN_LAYER_SIZE, r);
+		NN network = new NN(x.getColumns(), y.getColumns(), HIDDEN_LAYER_SIZE, HIDDEN_LAYER_AMOUNT, r);
 		
 		long start = System.currentTimeMillis();
 		
