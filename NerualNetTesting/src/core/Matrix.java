59e9eba7daf9fbab1257cee41b25694f365a7cc2
Jamie Stirling <jamiestirling2000@gmail.com>
2017-04-30 18:50:26 +0100

    Fixed assertions to align with transpose state

--- NerualNetTesting/src/core/Matrix.java
+++ NerualNetTesting/src/core/Matrix.java
@@ -255,13 +255,15 @@ public class Matrix implements Serializable {
 	
 	public static Matrix dot(Matrix a, Matrix b, Matrix out, boolean aT, boolean bT) {
 		
-		assert a.columns == b.rows && out.rows == a.rows && out.columns == b.columns;
+		
 		
 		assert !(aT && bT);
 		
 		int mdim, ndim, pdim;
 		
 		if (!aT && !bT) {
+			
+			assert a.columns == b.rows && out.rows == a.rows && out.columns == b.columns;
 			
 			mdim = a.getRows();
 			ndim = b.getColumns();
@@ -279,9 +281,10 @@ public class Matrix implements Serializable {
 			local[0] = 1;
 			
 			clEnqueueNDRangeKernel(commandQueue, dotKernel, 2, null, global, null, 0, null, null);
-		}
-		
-		if (aT) {
+			
+		} else if (aT) {
+			
+			assert a.rows == b.rows && out.rows == a.columns && out.columns == b.columns;
 			
 			mdim = a.getColumns();
 			ndim = b.getColumns();
@@ -300,8 +303,10 @@ public class Matrix implements Serializable {
 			
 			clEnqueueNDRangeKernel(commandQueue, dotATKernel, 2, null, global, null, 0, null, null);
 			
-		}
-		else if (bT){
+		} else if (bT) {
+			
+			assert a.columns == b.columns && out.rows == a.rows && out.columns == b.rows;
+			
 			mdim = a.getRows();
 			ndim = b.getRows();
 			pdim = a.getColumns();
